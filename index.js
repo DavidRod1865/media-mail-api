696b6de770f805e42d45af484f49043696a2b5e3
David Rodriguez <David.Rod1865@gmail.com>
2022-12-02 22:34:54 -0500

    updated Cors

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const PORT = 8000
 const cors = require('cors')
 
 app.use(express.json())
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://media-mail-api.vercel.app/api/weight/"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 let mediaMail = [
     {
